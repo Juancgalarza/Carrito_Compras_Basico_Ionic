353ca806d9e5015acb5a5dbd497e6aecd67b7929
JuanCa <juanca_20201@outlook.es>
2023-12-14 23:07:43 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppCarritoCompras',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
